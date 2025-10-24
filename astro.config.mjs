9e6dc69b70795f6c038bee91bfc4162be7ff43c9
Renchie <renchieyang@gmail.com>
2025-10-24 12:25:02 +1100

    added meta tag info to improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+const { SITE_ADDRESS } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_ADDRESS || "http://localhost:4321",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
