61ceb035558f7406f7e51bd2eac634befb21ad98
Renchie Yang <renchieyang@gmail.com>
2025-11-08 16:07:35 +1100

    added sitemap and robots.txt generation for enhanced SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 const { SITE_ADDRESS } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_ADDRESS || "http://localhost:4321",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
